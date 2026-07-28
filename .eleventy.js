9e1a6293fa340a794447000f0f2898d7b221ea60
Aiden Vaines <aiden@vaines.org>
2026-07-28 09:16:28 +0100

    adding favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 
   return {
     dir: {
